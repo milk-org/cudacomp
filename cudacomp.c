4f4eae1d796c45ba887ffa768dad896b209f9cc6
guyon <oliv.guyon@gmail.com>
2018-06-22 18:57:38 -1000

    timing tests

--- cudacomp.c
+++ cudacomp.c
@@ -991,7 +991,11 @@ void *compute_function( void *ptr )
                 sem_getvalue(gpumatmultconf[index].semptr1[device], &semval);
                 for(cnt=0; cnt<semval; cnt++)
                 {
+					printf("================================================\n");
+					printf("================================================\n");
 					printf("======== %s  %s  semptr1 decrement ============\n", __FILE__, __LINE__);
+					printf("================================================\n");
+					printf("================================================\n");
 					sem_trywait(gpumatmultconf[index].semptr1[device]);					
 				}
             }
