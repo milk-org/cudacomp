d2e227a423fe4356eb296a109098e8e8a88fe67c
guyon <oliv.guyon@gmail.com>
2018-08-25 12:25:59 -1000

    improved process management

--- cudacomp_MVMextractModesLoop.c
+++ cudacomp_MVMextractModesLoop.c
@@ -978,37 +978,37 @@ int  __attribute__((hot)) CUDACOMP_MVMextractModesLoop(
 			
 			if(data.signal_INT == 1){
 				sprintf(msgstring, "Received SIGINT at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 			
 			if(data.signal_TERM == 1){
 				sprintf(msgstring, "Received SIGTERM at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 			
 			if(data.signal_ABRT == 1){
 				sprintf(msgstring, "Received SIGABRT at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 			
 			if(data.signal_BUS == 1){
 				sprintf(msgstring, "Received SIGBUS at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 			
 			if(data.signal_SEGV == 1){
 				sprintf(msgstring, "Received SIGSEGV at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 			
 			if(data.signal_HUP == 1){
 				sprintf(msgstring, "Received SIGHUP at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}
 
 			if(data.signal_PIPE == 1){
 				sprintf(msgstring, "Received SIGPIPE at %02d:%02d:%02d.%09ld", tstoptm->tm_hour, tstoptm->tm_min, tstoptm->tm_sec, tstop.tv_nsec);
-				strcpy(processinfo->statusmsg, msgstring);
+				strncpy(processinfo->statusmsg, msgstring, 200);
 			}			
 			
 			
